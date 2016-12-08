e344aefaa5c196b69219e774f0e539c869028153
Max Camey <max.camey@huskers.unl.edu>
2016-12-08 16:55:06 -0600

    Changed Default Album
    Changed the  default album in the javascript file.

--- assets/js/album.js
+++ assets/js/album.js
@@ -2,9 +2,9 @@
  * This global array holds our collection of albums
  */
 var albums = [{
-  title: "The White Album",
-  band:  "The Beatles",
-  year:  1968
+  title: "Tommy",
+  band:  "The Who",
+  year:  1969
 }];
 
 
